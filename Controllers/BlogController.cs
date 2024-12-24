31098ee8f32b3ca40974e7b640a578c860f628c7
phanvantien2907 <vantienp530@gmail.com>
2024-12-25 00:33:01 +0700

    cập nhật blog, chi tiết blog

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Flower.Models;
+
+namespace Flower.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly FlowerContext _context;
+
+        public BlogController(FlowerContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/blog/{alias}-{id}.html")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.TbBlogs == null)
+            {
+                return NotFound();
+            }
+
+            var blogs = await _context.TbBlogs.FirstOrDefaultAsync(m => m.BlogId == id);
+
+            if (blogs == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.blogRelated =  _context.TbBlogs.Where(i=> i.BlogId != id).Take(4).ToList();
+
+            return View(blogs);
+        }
+    }
+}
