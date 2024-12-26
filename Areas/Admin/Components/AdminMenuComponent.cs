628de2b405e70c09355a93df6baf22ee3ad4f940
phanvantien2907 <vantienp530@gmail.com>
2024-12-26 15:44:54 +0700

    hiển thị menu admin, sửa 1 số lỗi vặt

---
+++ Areas/Admin/Components/AdminMenuComponent.cs
@@ -0,0 +1,24 @@
+﻿using Flower.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Flower.Areas.Admin.Components
+{
+    [ViewComponent(Name = "AdminMenu")]
+    public class AdminMenuComponent : ViewComponent
+    {
+        private readonly FlowerContext _context;
+
+        public AdminMenuComponent(FlowerContext db)
+        {
+            _context = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            var mnlist = _context.TbAdminMenus.Where(mn => mn.IsActive == true).ToList();
+            return await Task.FromResult((IViewComponentResult)View("Default", mnlist));
+        }
+    }
+}
